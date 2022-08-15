8a4724ddceb73be046a3447f476dc7fcb4559d7d
Glatrix <73367967+Glatrix@users.noreply.github.com>
2022-08-14 19:36:56 -0500

    Update Program.cs

--- MonoPipeSharp/Program.cs
+++ MonoPipeSharp/Program.cs
@@ -15,7 +15,7 @@ namespace MonoPipeSharp
         {
             Process p = Process.GetProcessesByName("Among Us")[0];
             MonoPipe.Is64Bit = false; //set if the process, is 32 or 64 bit NEEDED
-            Inject.InjectMonoCollector(p, 100); //Inject the dll (which dll depends on x64 value)
+            Inject.InjectMonoCollector(p, 100); //Inject the dll (which dll, depends on x64 value)
             MonoPipe.pipe_init(p); // init the client-side named pipe.
             MonoPipe.pipe_get_domains(); //collect all domains, to be stored inside the domains list.
             MonoPipe.pipe_set_domain(MonoPipe.domains[0]); //set the current domain on the serverpipe
